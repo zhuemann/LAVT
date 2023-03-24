1255e8ba5753ea48b1d093fbb69a1ed97e17ed8d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-03-24 18:02:16 -0500

    Adding candid dataloading info

--- candid_dataloader.py
+++ candid_dataloader.py
@@ -219,7 +219,7 @@ class TextImageDataset(Dataset):
             'images': image
         }
         """
-        return image, segmentation_mask, torch.tensor(ids, dtype=torch.long), torch.tensor(mask, dtype=torch.long)
+        return image, torch.tensor(segmentation_mask, dtype=torch.long), torch.tensor(ids, dtype=torch.long), torch.tensor(mask, dtype=torch.long)
 
 
     def shuffledTextAugmentation(text):
