2cf95af0c49cd567db0a9d842ef1861372aeb4a7
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-03-24 17:42:23 -0500

    Adding candid dataloading info

--- train.py
+++ train.py
@@ -153,6 +153,7 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, epoc
         else:
             output = model(image, sentences, l_mask=attentions)
 
+        print(output.size())
         loss = criterion(output, target)
         optimizer.zero_grad()  # set_to_none=True is only available in pytorch 1.6+
         loss.backward()
