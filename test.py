1ed1127ff076bcd564372ee645678e6436ed8af4
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-08-25 13:20:44 -0500

    Adding saving images to dgx

--- test.py
+++ test.py
@@ -102,6 +102,7 @@ def evaluate(model, data_loader, bert_model, device):
                     output_mask = output_item[0,:,:] + output_item[1,:,:]
                     #output_mask = np.expand_dims(output_mask, 0)
                     print(f"output_mask: {output_mask.shape}")
+                    print(f"type: {type(output_mask)}")
                     pred_rle = mask2rle(output_mask)
                     target_rle = mask2rle(target_item)
                     ids_example = row_ids[i * 8 + j]
