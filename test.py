139c0aed8024cf7f102003ae1e278038cfb55d3d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-08-25 14:25:03 -0500

    Adding saving images to dgx

--- test.py
+++ test.py
@@ -113,9 +113,9 @@ def evaluate(model, data_loader, bert_model, device):
                     #print(f"target_item size: {target_item.shape}")
 
                     target_rle = mask2rle(target_item)
-                    print(f"index: {i*8 + k}")
+                    print(f"index: {k}")
                     print(row_ids)
-                    ids_example = row_ids[(i * 8) + k]
+                    ids_example = row_ids[k]
 
                     pred_rle_list.append(pred_rle)
                     target_rle_list.append(target_rle)
