08e48182d5d283b3655a1e99081a97daf64967e0
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-11-16 18:26:00 -0600

    Running LAVT with empty string and frozen langauge model

--- train.py
+++ train.py
@@ -333,9 +333,11 @@ def main(args, dataset, dataset_valid):
         unfrozen_params = sum(param.numel() for param in model_obj.parameters() if param.requires_grad)
         return unfrozen_params
 
+    # [[p for p in single_model.text_encoder.encoder.layer[i].parameters() if p.requires_grad] for i in range(10)]
+
     print("Number of unfrozen parameters in model before:", count_unfrozen_parameters(model))
     print("Number of unfrozen parameters in bert before:", count_unfrozen_parameters(single_bert_model))
-    for param in single_bert_model.parameters():
+    for param in single_model.text_encoder.encoder.parameters():
         param.requires_grad = False
     print("Number of unfrozen parameters in bert after:", count_unfrozen_parameters(single_bert_model))
     print("Number of unfrozen parameters in model after:", count_unfrozen_parameters(model))
